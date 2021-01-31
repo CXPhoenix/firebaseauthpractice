67c8c8a4058a143344413ab1d1cec7f77643c9a2
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-01-31 10:54:18 +0800

    update loginsucess.html and new firebaseinit.js

---
+++ firebaseinit.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDp946q8Dcvt8vD0lTn__IsG--GLj4jGGc",
+    authDomain: "realisticpuzzle.firebaseapp.com",
+    projectId: "realisticpuzzle",
+    storageBucket: "realisticpuzzle.appspot.com",
+    messagingSenderId: "368962359768",
+    appId: "1:368962359768:web:7c10a33f73ad09a32762ad",
+    measurementId: "G-0ZT3CTZ9HV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
